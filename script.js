ae2e7d3ca6c42a9bed513cd9ba675cbc493c8c73
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-11-17 21:11:33 -0500

    validation and regex using if blocks

---
+++ script.js
@@ -0,0 +1,63 @@
+let form = document.getElementById('form');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let password2 = document.getElementById('password2');
+
+// show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// show sucess outline
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// check valid email
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// Event Listener
+form.addEventListener('submit', function(e) {
+    // console.log(username.value);
+
+    e.preventDefault();
+
+    if(username.value === ''){
+        showError(username, 'Username is required');
+    }
+    else {
+        showSuccess(username);
+    }
+
+    if(email.value === ''){
+        showError(email, 'Email is required');
+    }
+    else if(!isValidEmail(email.value)){
+        showError(email, 'Email is not valid')
+    }
+    else {
+        showSuccess(email);
+    }
+
+    if(password.value === ''){
+        showError(password, 'Password is required');
+    }
+    else {
+        showSuccess(password);
+    }
+
+    if(password2.value === ''){
+        showError(password2, 'Password is required');
+    }
+    else {
+        showSuccess(password2);
+    }
+});
